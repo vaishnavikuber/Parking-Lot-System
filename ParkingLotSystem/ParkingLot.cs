9af4c9786ed87381a8343321ef84e804bbc968ab
vaishnavi <vaishnavikuber2712@gmail.com>
2025-01-03 15:49:07 +0530

    [vaishnavi]added: parking lot project

---
+++ ParkingLotSystem/ParkingLot.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParkingLotSystem
+{
+    internal class ParkingLot:IParking
+    {
+        public int LotCapacity = 100;
+
+        public int AllocatedLot = 0;
+        
+
+        
+        public  bool ParkingAvailabilityStatus()
+        {
+            
+            if(AllocatedLot <= LotCapacity)
+            {
+                Console.WriteLine($"Lots are available: {LotCapacity - AllocatedLot}");
+                return true;
+               
+            }
+            else
+            {
+                Console.WriteLine("Lots are Full");
+                return false;
+            }
+        }
+
+        public int AssignLot()
+        {
+            if (ParkingAvailabilityStatus())
+            {
+                Console.WriteLine("Are you Handicap? Type yes or No");
+                string handicap = Console.ReadLine().ToLower();
+                if (handicap == "yes")
+                {
+                    Console.WriteLine("Special services are available for you");
+
+                }
+
+                AllocatedLot += 1;
+                Console.WriteLine("lot allocated");
+                return AllocatedLot;
+            }
+            else
+            {
+                Console.WriteLine("Can't assign ,Lots are Full");
+                return -1;
+            }
+        }
+
+
+
+
+
+    }
+}
---
+++ ParkingLotSystem/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ParkingLotSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What You want to do?");
+            Console.WriteLine("1. Park Car");
+            Console.WriteLine("2. Find Car");
+            Console.WriteLine("3. UnPark Car");
+            Console.WriteLine("4. Charges of Parking");
+            Console.WriteLine("5: Exit");
+
+            int choice =int.Parse( Console.ReadLine());
+
+            Console.WriteLine("Enter numberPlate of car");
+            string plate = Console.ReadLine();
+
+            Driver driver = new Driver();
+            switch (choice)
+            {
+                
+                case 1:
+                   
+                    Console.WriteLine("Enter vehicle color");
+                    string color= Console.ReadLine();
+                    Console.WriteLine("Enter vehicle make");
+                    string make = Console.ReadLine();
+                    Console.WriteLine("Enter vehicle size (large or small)");
+                    string size = Console.ReadLine().ToLower();
+                    driver.ParkCar(plate, size, color, make);
+                    break;
+
+                case 2:
+                    driver.FindCar(plate);
+                    break;
+
+                case 3:
+                   
+                    driver.UnParkCar(plate);
+                    break;
+
+                case 4:
+                 
+                    driver.ChargeOfParking(plate);
+                    break;
+
+                default: break;
+            }
+        }
+    }
+}
